9f22aa09c100489a5a060746fc4bcb18ad63bacd
Ardhi Lukianto <ardhi@lukianto.com>
2025-01-05 21:41:43 +0700

    Updates

---
+++ bajo/method/search.js
@@ -0,0 +1,18 @@
+async function search (params = {}, extra = {}) {
+  const { pick } = this.app.bajo.lib._
+  const { fetch } = this.app.bajoExtra
+  const endpoint = `${this.config.remoteUrl}/search`
+  const opts = {
+    query: {}
+  }
+  if (params.query) {
+    opts.query = pick(params, ['limit'])
+    opts.query.q = params.query
+  } else opts.query = pick(params, ['limit', 'amenity', 'street', 'city', 'county', 'state', 'country', 'postalcode'])
+  opts.query.format = 'json'
+  opts.query.limit = opts.query.limit ?? this.config.limit
+  if (extra.polygon) opts.query.polygon_geojson = 1
+  return await fetch(endpoint, opts, { agent: this.config.userAgent })
+}
+
+export default search
---
+++ bajoCli/applet/search-freeform.js
@@ -0,0 +1,28 @@
+async function searchFreeform (path, ...args) {
+  const { importPkg } = this.app.bajo
+  const { isEmpty } = this.app.bajo.lib._
+  const { search } = this.app.sumbaNominatim
+  const input = await importPkg('bajoCli:@inquirer/input')
+  const { getOutputFormat, writeOutput } = this.app.bajoCli
+  const format = getOutputFormat()
+  let query = args[0]
+  if (isEmpty(query)) {
+    query = await input({
+      message: this.print.write('Please enter a query:')
+    })
+  }
+  if (isEmpty(query)) return this.print.fail('Query must be provided!', { exit: this.app.bajo.applet })
+  const spinText = 'Fetching...'
+  const spin = this.print.spinner({ showCounter: true }).start(spinText)
+
+  let result
+  try {
+    result = await search({ query })
+    spin.info('Done!')
+  } catch (err) {
+    spin.fatal('Error: %s', err.message)
+  }
+  await writeOutput(result, path, format)
+}
+
+export default searchFreeform
