24764984ba6946b878720ff4307d1133c1a299f7
Ardhi Lukianto <ardhi@lukianto.com>
2025-08-23 23:43:31 +0700

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 async function factory (pkgName) {
   const me = this
 
-  return class SumbaNominatim extends this.lib.BajoPlugin {
+  return class SumbaNominatim extends this.lib.Plugin {
     constructor () {
       super(pkgName, me.app)
       this.alias = 'snom'
